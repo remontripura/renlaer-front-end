f6548ec5f088b96fb86ff1da3c9021fba6d5b115
Remon Tripura <remontripura045@gmail.com>
2023-09-23 09:44:39 +0600

    problem fixed

--- src/pages/shared/Navbar.jsx
+++ src/pages/shared/Navbar.jsx
@@ -12,6 +12,7 @@ const Navbar = () => {
   const [isIconOpen, setIsIconOpen] = useState(false);
   const toggleDropdown = () => {
     setIsOpen(!isOpen);
+    
   };
   const rotateIcon = () => {
     setIsIconOpen(!isIconOpen);
@@ -23,11 +24,25 @@ const Navbar = () => {
           Home
         </Link>
       </li>
-      <li
-        className="group opacity-100 flex items-center gap-2"
-        onClick={toggleDropdown}
-      >
-        Product
+      <li className="group opacity-100 flex items-center gap-2" onClick={toggleDropdown}>
+        <span className="hidden lg:flex">
+          Products
+        </span>
+        <div
+          onClick={rotateIcon}
+          className="flex justify-center items-center gap-2 lg:hidden group"
+        >
+          Product
+          {isIconOpen ? (
+            <FaAngleDown
+              className="text-[20px]"
+            />
+          ) : (
+            <FaAngleUp
+              className="text-[20px]"
+            />
+          )}
+        </div>
         <div className="absolute group left-80 invisible lg:group-hover:visible opacity-100 top-20">
           <div className="w-6 h-6 bg-white rotate-45 absolute -top-3 left-48"></div>
           <ul className="lg:grid grid-cols-3 max-w-[700px] p-8 gap-10 relative bg-white text-black">
@@ -40,19 +55,6 @@ const Navbar = () => {
           </ul>
         </div>
         <FaAngleDown className="text-[20px] hidden lg:flex lg:group-hover:rotate-180 duration-500" />
-        <div onClick={rotateIcon} className="flex lg:hidden group">
-          {isIconOpen ? (
-            <FaAngleDown
-              onClick={rotateIcon}
-              className="text-[20px] lg:flex duration-500"
-            />
-          ) : (
-            <FaAngleUp
-              onClick={rotateIcon}
-              className="text-[20px] duration-500"
-            />
-          )}
-        </div>
       </li>
       {isOpen && (
         <div className="opacity-100">
@@ -92,35 +94,6 @@ const Navbar = () => {
 
   return (
     <div className=" bg-[#131947] relative">
-      {/* <div className="navbar lg:max-w-7xl mx-auto text-white">
-                <div className="navbar-start">
-                    <Link to="/"><img className="w-24" src={logo} alt="" /></Link>
-                </div>
-                <div className="navbar-center lg:flex ">
-                    <ul className="menu menu-horizontal lg:flex gap-7 hidden px-1">
-                        {navItem}
-                    </ul>
-                </div>
-                <div className="navbar-end lg:flex ">
-                    <div className="dropdown dropdown-end ">
-                        <label tabIndex={0} className="btn btn-ghost lg:hidden">
-                            <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
-                        </label>
-                        <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 p-2 shadow rounded-box w-52 bg-[#1a4bff] text-white">
-                            {navItem}
-                            <div className="lg:hidden flex flex-col gap-2">
-                                <button className="bg-[#5177FF] py-2 rounded-xl font-semibold text-white hover:bg-[#1a4bff] w-full">Login</button>
-                                <button className="bg-[#5177FF] py-2 rounded-xl font-semibold text-white hover:bg-[#1a4bff] w-full">Sign Up</button>
-                            </div>
-                        </ul>
-
-                    </div>
-                    <div className="lg:flex gap-2 hidden">
-                        <button className="bg-[#5177FF] py-2 rounded font-semibold text-white hover:bg-[#1a4bff] w-[100px]">Login</button>
-                        <button className="bg-[#5177FF] py-2 rounded font-semibold text-white hover:bg-[#1a4bff] w-[100px]">Sign Up</button>
-                    </div>
-                </div>
-            </div> */}
       <div className="lg:max-w-7xl mx-auto text-white flex justify-between items-center px-4 py-3">
         <div className="">
           <Link to="/">
@@ -166,26 +139,3 @@ const Navbar = () => {
 };
 
 export default Navbar;
-
-{
-  /* <div className="absolute top-0 -left-[400px] transition group-hover:translate-y-5 translate-y-0 opacity-0 invisible group-hover:opacity-100 group-hover:visible duration-500 ease-in-out group-hover:transform z-50 min-w-[1200px] transform">
-                <div className="relative top-3 p-6 bg-white rounded-xl shadow-xl w-full">
-                    <div className="w-10 h-10 bg-white transform rotate-45 absolute top-0 z-0 translate-x-0 transition-transform group-hover:translate-x-[23rem] duration-500 ease-in-out rounded-sm">
-                    </div>
-                    <div className="relative z-10">
-                        <div className="grid grid-cols-3 gap-3">
-                            {
-                                scanario.map(data => <div key={data.id} className="text-black">
-                                    <div className="card bg-base-100 shadow-xl">
-                                        <div className="card-body">
-                                            <h2 className="font-bold">{data.title}</h2>
-                                            <p>{data.description}</p>
-                                        </div>
-                                    </div>
-                                </div>)
-                            }
-                        </div>
-                    </div>
-                </div>
-            </div> */
-}
